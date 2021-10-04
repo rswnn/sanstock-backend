8cbfbdf82207102cbb19b33db00b86399afa3584
yusufekoanggoro <anggoroekoyusuf@gmail.com>
2021-10-04 21:47:21 +0700

    add require

--- bin/modules/user/v1/repositories/commands/command.js
+++ bin/modules/user/v1/repositories/commands/command.js
@@ -1,7 +1,8 @@
 const Mysql = require('../../../../../infrastructure/databases/mysql/db');
+const configs = require('../../../../../infrastructure/configs/global_config');
 
 const insertUser = async (param) => {
-    const db = new Mysql(config.get('/mysqlConfig'));
+    const db = new Mysql(configs.get('/mysqlConfig'));
     const query = ``
     const result = await db.query(query, [param]);
     return result;
