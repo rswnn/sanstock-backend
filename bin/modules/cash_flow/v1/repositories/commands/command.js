7ce7452f1cff46c91f3b2c66c2ae60898287b29b
yusufekoanggoro <anggoroekoyusuf@gmail.com>
2021-10-19 16:30:19 +0700

    add sale

--- bin/modules/cash_flow/v1/repositories/commands/command.js
+++ bin/modules/cash_flow/v1/repositories/commands/command.js
@@ -15,6 +15,16 @@ const insertCash = async (param) => {
   return result;
 };
 
+const insertCashFromAddSale = async (param) => {
+  const { dateTime, type, cashIn, cashOut, description, createdAt, updatedAt } = param;
+  const db = new Mysql(configs.get('/mysqlConfig'));
+  const query = `INSERT INTO cash_flow (id, date_time, type, cash_in, cash_out, description, created_at, updated_at)
+  VALUES (NULL, '${dateTime}', '${type}', '${Number(cashIn)}', '${Number(cashOut)}', '${description}', '${createdAt}', '${updatedAt}')`;
+  const result = await db.query(query);
+  return result;
+};
+
 module.exports = {
-  insertCash
+  insertCash,
+  insertCashFromAddSale
 };
--- bin/modules/sales/v1/repositories/commands/domain.js
+++ bin/modules/sales/v1/repositories/commands/domain.js
@@ -1,5 +1,6 @@
 const wrapper = require('../../../../../helpers/utils/wrapper');
 const command = require('../commands/command');
+const commandCashFLow = require('../../../../cash_flow/v1/repositories/commands/command');
 const moment = require('moment-timezone');
 moment.tz('Asia/Jakarta');
 
@@ -10,6 +11,16 @@ class Sale {
     const insertSale = await command.insertSale(payload);
     if (insertSale.err) {
       return wrapper.error('err', insertSale.message, insertSale.code);
+    }
+    payload.dateTime = payload.createdAt;
+    payload.type = 1;
+    payload.cashIn = payload.hargaJual;
+    payload.cashOut = 0;
+    payload.total = 1000;
+    payload.description = "Tambah Income";
+    const insertCashFlow = await commandCashFLow.insertCashFromAddSale(payload)
+    if (insertCashFlow.err) {
+      return wrapper.error('err', insertCashFlow.message, insertCashFlow.code);
     }
     return wrapper.data('', 'Success Input', 201);
   }
