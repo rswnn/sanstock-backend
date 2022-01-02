f16e86c6efb7d1a991f7376599fb065dff24798b
Arif <ariffad0196@gmail.com>
2022-01-02 20:05:48 +0700

    addjust query

--- bin/modules/transactions/v1/repositories/queries/query.js
+++ bin/modules/transactions/v1/repositories/queries/query.js
@@ -3,7 +3,7 @@ const configs = require('../../../../../infrastructure/configs/global_config');
 
 const listProduct = async () => {
   const db = new Mysql(configs.get('/mysqlConfig'));
-  const query = 'SELECT * FROM products';
+  const query = 'SELECT products.*, users.username as userName, suppliers.id as supplierId, suppliers.nama as supplierName FROM sales LEFT OUTER JOIN products ON products.product_id = products.id';
   const result = await db.query(query);
   return result;
 };
