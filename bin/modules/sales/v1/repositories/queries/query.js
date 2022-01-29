7d36f3f7b97fdc5a143be1c72f4be483e8df86e9
riswan ardiansah <riswanardiansah@riswans-MacBook-Pro-2.local>
2022-01-29 17:59:21 +0700

    adjust bug

--- bin/modules/sales/v1/repositories/queries/query.js
+++ bin/modules/sales/v1/repositories/queries/query.js
@@ -10,7 +10,7 @@ const listSale = async () => {
 
 const countSalesBySKU = async (sku) => {
   const db = new Mysql(configs.get('/mysqlConfig'));
-  const query = `SELECT COUNT(*) FROM sales LEFT JOIN users ON user_id = users.id LEFT JOIN products ON product_id = products.id LEFT JOIN merchants ON merchant_id = merchants.id WHERE sku = '${sku}'`;
+  const query = `SELECT COUNT(*) FROM sales LEFT JOIN users ON user_id = users.id LEFT JOIN products ON product_id = products.id LEFT JOIN merchants ON merchant_id = merchants.id WHERE sku = '${sku}' AND transaction_type = 'outcome' `;
   const result = await db.query(query);
   return result;
 };
